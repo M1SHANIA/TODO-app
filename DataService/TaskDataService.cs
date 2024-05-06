bfefbbc9adb161fc4eefc56edde241c630f7421a
Mykhailo Onipchenko <mihaonipchenko@gmail.com>
2024-05-07 01:57:28 +0200

    json integration, but need to change to system.json!!!

---
+++ DataService/TaskDataService.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using WpfApp1.Models;
+
+namespace WpfApp1.DataService
+{
+    public class TaskDataService
+    {
+        // Define file path, folder name and file name
+        private readonly string _filePath;
+        private readonly string folderName = "WpfApp1";
+        private readonly string fileName = "tasks.json";
+
+        public TaskDataService()
+        {
+            // Get the path to the Application Data folder
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            // Combine the Application Data path with the folder name
+            string appFolder = Path.Combine(appDataPath, folderName);
+            // Combine the app folder path with the "data" folder
+            string dataFolder = Path.Combine(appFolder, "data");
+            // If the data folder doesn't exist, create it
+            if (!Directory.Exists(dataFolder))
+            {
+                Directory.CreateDirectory(dataFolder);
+            }
+            // Combine the data folder path with the file name to get the full file path
+            _filePath = Path.Combine(dataFolder, fileName);
+
+            // Initialize the file
+            InitializeFile();
+        }
+
+        private void InitializeFile()
+        {
+            // If the file doesn't exist, create it and write an empty JSON array to it
+            if (!File.Exists(_filePath))
+            {
+                File.WriteAllText(_filePath, JsonConvert.SerializeObject(new List<Task>()));
+            }
+            // Open the folder containing the file (for debugging purposes)
+            Process.Start(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), folderName));
+        }
+
+        // Load tasks from the file
+        public List<Task> LoadTasks()
+        {
+            string fileContent = File.ReadAllText(_filePath);
+            return JsonConvert.DeserializeObject<List<Task>>(fileContent);
+        }
+
+        // Save tasks to the file
+        public void SaveTasks(List<Task> tasks)
+        {
+            string json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+            File.WriteAllText(_filePath, json);
+        }
+
+        // Add a new task to the file
+        public void AddTask(Task newTask)
+        {
+            // Generate an ID for the new task
+            newTask.Id = GenerateId();
+
+            // Load the existing tasks
+            var task = LoadTasks();
+            // Add the new task to the list
+            task.Add(newTask);
+            // Save the updated list back to the file
+            SaveTasks(task);
+        }
+
+        public void UpdateTask(Task updatedTask)
+        {
+            // Load the existing tasks
+            var tasks = LoadTasks();
+            // Find the task to update
+            var taskIndex = tasks.FindIndex(t => t.Id == updatedTask.Id);
+            // If the task was found, update it
+            if (taskIndex != -1)
+            {
+                tasks[taskIndex] = updatedTask;
+                SaveTasks(tasks);
+            }
+            // Save the updated list back to the file
+            SaveTasks(tasks);
+        }
+
+        public void DeleteTask(int taskId)
+        {
+            // Load the existing tasks
+            var tasks = LoadTasks();
+            // Find the task to delete
+            var taskIndex = tasks.FindIndex(t => t.Id == taskId);
+            // If the task was found, delete it
+            if (taskIndex != -1)
+            {
+                tasks.RemoveAll(t => t.Id == taskId);
+                SaveTasks(tasks);
+            }
+        }   
+
+        // Generate a new ID for a task
+        public int GenerateId()
+        {
+            // Load the existing tasks
+            var tasks = LoadTasks();
+
+            // If there are no tasks, return 1
+            if (!tasks.Any())
+            {
+                return 1;
+            }
+
+            // Otherwise, return one more than the maximum existing ID
+            int maxId = tasks.Max(t => t.Id);
+            return maxId + 1;
+        }
+    }
+}
---
+++ ViewModels/TaskViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using WpfApp1.DataService;
+using WpfApp1.Models;
+
+namespace WpfApp1.ViewModels
+{
+    // ViewModel for the Task model
+    public class TaskViewModel : INotifyPropertyChanged
+    {
+        // Instance of the TaskDataService
+        private readonly TaskDataService _taskDataService;
+
+        // Collection of tasks
+        private ObservableCollection<Task> _tasks;
+
+        // Property for the tasks collection
+        public ObservableCollection<Task> Tasks
+        {
+            get => _tasks;
+            set
+            {
+                _tasks = value;
+                OnPropertyChanged(nameof(Tasks));
+            }
+        }
+
+        // Event for property changed
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Method to load tasks from the data service
+        private void LoadTasks()
+        {
+            var taskList = _taskDataService.LoadTasks();
+            Tasks = new ObservableCollection<Task>(taskList);
+        }
+
+        // Method to add a new task using the data service
+        public void AddNewTask(Task newTask)
+        {
+            _taskDataService.AddTask(newTask);
+            LoadTasks();
+        }
+
+        // Method to update a task using the data service
+        public void UpdateTask(Task updatedTask)
+        {
+            _taskDataService.UpdateTask(updatedTask);
+            LoadTasks();
+        }
+
+        // Method to delete a task using the data service
+        public void DeleteTask(int taskId)
+        {
+            _taskDataService.DeleteTask(taskId);
+            LoadTasks();
+        }
+
+        // Constructor for the ViewModel
+        TaskViewModel()
+        {
+            _taskDataService = new TaskDataService();
+        }
+
+        // Method to invoke the PropertyChanged event
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
